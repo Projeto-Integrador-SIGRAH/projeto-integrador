32f1a6a43ca087477dcf56d72ca858472c382581
Adryan Eryk de Oliveira Leite <113716276+Adryan1928@users.noreply.github.com>
2026-09-25 15:09:46 -0300

    Some tailwind config

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,15 @@
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('user/', include('user.urls')),
+    path("admin/", admin.site.urls),
+    path("user/", include("user.urls")),
 ]
+
+
+if settings.DEBUG:
+    # Include django_browser_reload URLs only in DEBUG mode
+    urlpatterns += [
+        path("__reload__/", include("django_browser_reload.urls")),
+    ]
